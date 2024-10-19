2fd9afa0d3a8ef1756e32ce3bc9127278d198ceb
keyvan <keyvan.razavi20@gmail.com>
2024-10-19 23:00:39 +0330

    some changes

--- src/app/dashboard/admin/orders-management/page.tsx
+++ src/app/dashboard/admin/orders-management/page.tsx
@@ -1,8 +1,8 @@
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React from "react";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import TableThree from "@/components/Tables/OrdersList";
 import { Metadata } from "next";
+import OrdersList from "@/components/Tables/OrdersList";
 
 export const metadata: Metadata = {
   title: "Kolet: Orders-management",
@@ -14,7 +14,7 @@ const OrdersManagement = () => {
     <DefaultLayout>
       <Breadcrumb pageName="سفارشها" />
       <div className="min-h-svh xl:max-w-full">
-        <TableThree />
+        <OrdersList />
       </div>
     </DefaultLayout>
   );
